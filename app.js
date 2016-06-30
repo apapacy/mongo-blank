a08e1cf264d2b17957aa1f4f94f28aee3e664bb2
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-06-30 16:06:50 +0300

    mongodb3

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/models/Customer.ts
+++ app/models/Customer.ts
@@ -1,85 +1,81 @@
-import {Model, Instance, Collection, Index, Property, ObjectID, Core}  from 'iridium';
+'use strict';
+import {Core, Model, Instance, Collection, Index, Property, ObjectID} from 'iridium';
 
 interface Colour {
-  r: number;
-  g: number;
-  b: number;
+    r: number;
+    g: number;
+    b: number;
 }
 
 interface Car {
-  make: string;
-  model: string;
-  colour: Colour;
+    make: string;
+    model: string;
+    colour: Colour;
 }
 
 interface HouseDocument {
-  _id?: string;
-  name: string;
+    _id?: string;
+    name: string;
 
-  cars?: Car[];
+    cars?: Car[];
 }
-
-class Dummy{}
-
 
 @Index({ name: 1 })
 @Collection('houses')
 class House extends Instance<HouseDocument, House> implements HouseDocument {
-  @ObjectID _id: string;
-  @Property(/^.+$/)
-  name: string;
+    @ObjectID _id: string;
+    @Property(/^.+$/)
+    name: string;
 
-  @Property([{
-    make: String,
-    model: String,
-    colour: {
-      r: Number,
-      g: Number,
-      b: Number
+    @Property([{
+        make: String,
+        model: String,
+        colour: {
+            r: Number,
+            g: Number,
+            b: Number
+        }
+    }])
+    cars: Car[];
+
+    static onCreating(doc: HouseDocument) {
+        doc.cars = doc.cars || [];
     }
-  }])
-  cars: Car[];
 
-  static onCreating(doc: HouseDocument) {
-    doc.cars = doc.cars || [];
-  }
+    addCar(make: string, model: string, colour: Colour) {
+        this.cars.push({
+            make: make,
+            model: model,
+            colour: colour
+        });
+    }
 
-  addCar(make: string, model: string, colour: Colour) {
-    this.cars.push({
-      make: make,
-      model: model,
-      colour: colour
-    });
-  }
-
-  get numberOfCars() {
-    return this.cars.length;
-  }
+    get numberOfCars() {
+        return this.cars.length;
+    }
 }
 
 class MyDatabase extends Core {
-  Houses: Model<HouseDocument, House> = new Model<HouseDocument, House>(this, House);
+    Houses = new Model<HouseDocument, House>(this, House);
 }
 
-try {
-var myDb = new MyDatabase({ database: 'houses_test' });
+var myDb = new MyDatabase({host: 'localhost', database: 'houses_test' });
 
-myDb.connect().then(async function(){
-  for (var i:number=Number(0); i<1000000;i++) await myDb.Houses.insert({
-    name: 'My House-' + i.toString(),
-    cars: [{
-      make: 'Audi',
-      model: 'A4',
-      colour: { r: 0, g: 0, b: 0 }
-    }]
-})})
-  .then(() => myDb.Houses.get(),err=>console.log(err))
-  .then((house) => {
-  house.addCar('Audi', 'S4', { r: 255, g: 255, b: 255 });
-  return house.save();
-})
-  .then(() => myDb.close());
-
-} catch(ex) {
-console.log(ex)
-}
+myDb.connect().then(() =>
+  {
+    for (var i =0; i <100000; i++)
+    myDb.Houses.insert({
+        name: 'My House-' + i,
+        cars: [{
+            make: 'Audi',
+            model: 'A4',
+            colour: { r: 0, g: 0, b: 0 }
+        }]
+  })
+},err => console.log(err))
+    .then(() => myDb.Houses.get())
+    .then((house) => {
+        house.addCar('Audi', 'S4', { r: 255, g: 255, b: 255 });
+        return house.save();
+    },err => console.log(err))
+    .then(() =>  true/*myDb.close()*/,err => console.log(err))
